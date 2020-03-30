2bb3366593c8d357968fbc0bf8356366a2555223
akhil <akhilviswam000@gmail.com>
2020-03-31 00:51:47 +0530

    mux api setuped

--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bitly/go-simplejson"
+	"github.com/gorilla/mux"
 )
 
+func getVehicleData(w http.ResponseWriter, r *http.Request) {
+	json := simplejson.New()
+	json.Set("foo", "bar")
 
-func main(){
-	
+	payload, err := json.MarshalJSON()
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
+}
+func main() {
+	fmt.Println(".............")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/get-vehicle-data", getVehicleData).Methods("GET")
+
+	http.ListenAndServe(":8001", router)
+
 }
