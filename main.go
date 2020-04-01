f25ed6643872def5f4949e92878ca67cd7c05ede
akhil <akhilviswam000@gmail.com>
2020-04-01 23:58:26 +0530

    scraping code added

--- main.go
+++ main.go
@@ -5,11 +5,41 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/bitly/go-simplejson"
 	"github.com/gorilla/mux"
 )
 
-func getVehicleData(w http.ResponseWriter, r *http.Request) {
+// This will get called for each HTML element found
+func processElement(index int, element *goquery.Selection) {
+	// See if the href attribute exists on the element
+	fmt.Println("=================================")
+	fmt.Println(element.Text())
+	href, exists := element.Attr("href")
+	if exists {
+		fmt.Println(href)
+	}
+}
+
+func getCoronaData(w http.ResponseWriter, r *http.Request) {
+
+	// Make request
+	response, err := http.Get("https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Kerala")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	// Find all links and process them with the function
+	// defined earlier
+	document.Find("#mw-content-text > div > table.infobox > tbody > tr:nth-child(8) > td").Each(processElement)
+
 	json := simplejson.New()
 	json.Set("foo", "bar")
 
@@ -21,11 +51,12 @@ func getVehicleData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(payload)
 }
+
 func main() {
 	fmt.Println(".............")
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/get-vehicle-data", getVehicleData).Methods("GET")
+	router.HandleFunc("/api/get-vehicle-data", getCoronaData).Methods("GET")
 
 	http.ListenAndServe(":8001", router)
 
